30d58dfee0c93aadbb821424b3608c3dc88a3a6d
kanekonanami <n-kaneko@jutjoy.com>
2022-09-24 11:29:36 +0900

    4-16

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,16 +1,11 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
+@section('title', 'Myプロフィールの編集画面')
 
-
-{{-- profile.blade.phpの@yield('title')に'プロフィールの編集画面'を埋め込む --}}
-@section('title', 'プロフィールの編集画面')
-
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール編集画面</h2>
+                <h2>Myプロフィール編集画面</h2>
             </div>
         </div>
     </div>
